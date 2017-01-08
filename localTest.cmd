a172c316694980a31790c9411314986f673e2c69
Akhil Thomas <akhil.thomas@spektrasystems.com>
2017-01-08 16:53:33 +0530

    Update localTest.cmd

--- localTest.cmd
+++ localTest.cmd
@@ -8,7 +8,7 @@ REM The endpoint that our validate and deploy requests will be sent to
 set VALIDATION_HOST=http://qst9494f4f5c44dd94d84049.westus.cloudapp.azure.com
 
 REM Whether to skip the remote validate test or not.
-set VALIDATION_SKIP_VALIDATE=true
+set VALIDATION_SKIP_VALIDATE=false
 
 REM Whether to skip the remote deploy test or not.
 set VALIDATION_SKIP_DEPLOY=true
